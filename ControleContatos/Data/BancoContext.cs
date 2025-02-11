2014b2672e821b1838e1994835b0788db828f8ab
João Caetano Muniz Barros <joaocaetanomuniz1555@hotmail.com>
2025-02-11 20:07:04 -0300

    adição de pacotes ao projeto e criação do esqueleto do contexto do banco de dados.

---
+++ ControleContatos/Data/BancoContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ControleContatos.Data
+{
+    public class BancoContext : DbContext  //contexto do entity framework do banco de dados
+    {
+        public BancoContext(DbContextOptions <BancoContext> options) : base(options)//injetar como parametro de entrada o dbcontext e tipar ele como banco context, db options como parametro de entrada.
+        {
+            
+        }
+
+
+    }
+}
