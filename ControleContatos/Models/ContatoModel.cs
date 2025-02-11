0635d171dd2e3ded052f6d424a35a2c9b49e6dd3
João Caetano Muniz Barros <joaocaetanomuniz1555@hotmail.com>
2025-02-11 19:45:20 -0300

    criação da tabela contato

---
+++ ControleContatos/Models/ContatoModel.cs
@@ -0,0 +1,10 @@
+﻿namespace ControleContatos.Models
+{
+    public class ContatoModel
+    {
+        public int Id { get; set; } //vai representar o código do contato, vai ser enumerado.
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Celular { get; set; }
+    }
+}
