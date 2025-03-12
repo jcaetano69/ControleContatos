92d9b22649d2cca403a4fd164cbb884c6208b272
João Caetano Muniz Barros <joaocaetanomuniz1555@hotmail.com>
2025-03-12 14:38:38 -0300

    adicionando as validações a classe ContatoModel

--- ControleContatos/Models/ContatoModel.cs
+++ ControleContatos/Models/ContatoModel.cs
@@ -1,10 +1,20 @@
-﻿namespace ControleContatos.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ControleContatos.Models
 {
     public class ContatoModel
     {
         public int Id { get; set; } //vai representar o código do contato, vai ser enumerado.
+
+
+        //adicionando as validações ao contato model
+        [Required(ErrorMessage ="Digite o nome do contato")]
         public string Nome { get; set; }
+        [Required (ErrorMessage ="Digite o e-mail do contato")]
+        [EmailAddress(ErrorMessage = "O email informado não é válido")]
         public string Email { get; set; }
+        [Required (ErrorMessage ="Digite o celular do contato")]
+        [Phone(ErrorMessage ="O celular informado não é válido")]
         public string Celular { get; set; }
     }
 }
