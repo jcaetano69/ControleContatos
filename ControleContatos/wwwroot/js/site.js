6440419468bfb4cf2d49a39ef4518f32550cbdce
João Caetano Muniz Barros <joaocaetanomuniz1555@hotmail.com>
2025-03-24 12:12:34 -0300

    correção

--- ControleContatos/wwwroot/js/site.js
+++ ControleContatos/wwwroot/js/site.js
@@ -3,27 +3,23 @@
 
 // Write your JavaScript code.
 
+
 $(document).ready(function () {
-    $('table-contatos').DataTable();
+
+
+    //let table = new DataTable('#table-contatos');
+
+   //                    $('table-contatos').DataTable();
+    $('#table-contatos').DataTable({
+        "paging": true,          // Habilita a paginação
+        "pageLength": 3,        // Define o número de itens por página
+        "lengthChange": true,    // Permite ao usuário alterar o número de       itens por página
+        "searching": true,       // Habilita a funcionalidade de busca
+        "ordering": true,       // Habilita a ordenação das colunas
+        "info": true,           // Exibe informações sobre a paginação
+        "autoWidth": false       // Desabilita o ajuste automático da largura das colunas
+    });
 });
-
-let table = new DataTable('#table-contatos');
-
-@section scripts {
-    <script>
-        $(document).ready(function () {
-            $('#table-contatos').DataTable({
-                "paging": true,          // Habilita a paginação
-                "pageLength": 10,        // Define o número de itens por página
-                "lengthChange": true,    // Permite ao usuário alterar o número de itens por página
-                "searching": true,       // Habilita a funcionalidade de busca
-                "ordering": true,       // Habilita a ordenação das colunas
-                "info": true,           // Exibe informações sobre a paginação
-                "autoWidth": false       // Desabilita o ajuste automático da largura das colunas
-            });
-        });
-    </script>
-}
 
 $(' .close-alert').click(function () {
     $('.alert').hide('hide');
