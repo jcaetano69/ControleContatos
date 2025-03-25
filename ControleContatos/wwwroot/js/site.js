169f41af3f98d7351e7538f82da3231bb8cf604f
João Caetano Muniz Barros <joaocaetanomuniz1555@hotmail.com>
2025-03-24 23:08:46 -0300

    tradução da página para pt-br

--- ControleContatos/wwwroot/js/site.js
+++ ControleContatos/wwwroot/js/site.js
@@ -7,19 +7,47 @@
 $(document).ready(function () {
 
 
-    //let table = new DataTable('#table-contatos');
+   
+//    $('#table-contatos').DataTable({
+//        "paging": true,          // Habilita a paginação
+//        "pageLength": 3,        // Define o número de itens por página
+//        "lengthChange": true,    // Permite ao usuário alterar o número de       itens por página
+//        "searching": true,       // Habilita a funcionalidade de busca
+//        "ordering": true,       // Habilita a ordenação das colunas
+//        "info": true,           // Exibe informações sobre a paginação
+//        "autoWidth": false       // Desabilita o ajuste automático da largura das colunas
+//    });
+    //});
 
-   //                    $('table-contatos').DataTable();
+
     $('#table-contatos').DataTable({
-        "paging": true,          // Habilita a paginação
-        "pageLength": 3,        // Define o número de itens por página
-        "lengthChange": true,    // Permite ao usuário alterar o número de       itens por página
-        "searching": true,       // Habilita a funcionalidade de busca
-        "ordering": true,       // Habilita a ordenação das colunas
-        "info": true,           // Exibe informações sobre a paginação
-        "autoWidth": false       // Desabilita o ajuste automático da largura das colunas
+        "ordering": true,
+        "paging": true,
+        "searching": true,
+        "oLanguage": {
+            "sEmptyTable": "Nenhum registro encontrado na tabela",
+            "sInfo": "Mostrar _START_ até _END_ de _TOTAL_ registros",
+            "sInfoEmpty": "Mostrar 0 até 0 de 0 Registros",
+            "sInfoFiltered": "(Filtrar de _MAX_ total registros)",
+            "sInfoPostFix": "",
+            "sInfoThousands": ".",
+            "sLengthMenu": "Mostrar _MENU_ registros por pagina",
+            "sLoadingRecords": "Carregando...",
+            "sProcessing": "Processando...",
+            "sZeroRecords": "Nenhum registro encontrado",
+            "sSearch": "Pesquisar",
+            "oPaginate": {
+                "sNext": "Proximo",
+                "sPrevious": "Anterior",
+                "sFirst": "Primeiro",
+                "sLast": "Ultimo"
+            },
+            "oAria": {
+                "sSortAscending": ": Ordenar colunas de forma ascendente",
+                "sSortDescending": ": Ordenar colunas de forma descendente"
+            }
+        }
     });
-});
 
 $(' .close-alert').click(function () {
     $('.alert').hide('hide');
