cedf29ca30f55f34780905861ceed9514769e4e0
João Caetano Muniz Barros <joaocaetanomuniz1555@hotmail.com>
2025-03-24 23:13:07 -0300

    alteração final na paginação

--- ControleContatos/wwwroot/js/site.js
+++ ControleContatos/wwwroot/js/site.js
@@ -8,16 +8,16 @@ $(document).ready(function () {
 
 
    
-//    $('#table-contatos').DataTable({
-//        "paging": true,          // Habilita a paginação
-//        "pageLength": 3,        // Define o número de itens por página
-//        "lengthChange": true,    // Permite ao usuário alterar o número de       itens por página
-//        "searching": true,       // Habilita a funcionalidade de busca
-//        "ordering": true,       // Habilita a ordenação das colunas
-//        "info": true,           // Exibe informações sobre a paginação
-//        "autoWidth": false       // Desabilita o ajuste automático da largura das colunas
-//    });
-    //});
+    $('#table-contatos').DataTable({
+        "paging": true,          // Habilita a paginação
+        "pageLength": 3,        // Define o número de itens por página
+        "lengthChange": true,    // Permite ao usuário alterar o número de       itens por página
+        "searching": true,       // Habilita a funcionalidade de busca
+        "ordering": true,       // Habilita a ordenação das colunas
+        "info": true,           // Exibe informações sobre a paginação
+        "autoWidth": false       // Desabilita o ajuste automático da largura das colunas
+    });
+    });
 
 
     $('#table-contatos').DataTable({
