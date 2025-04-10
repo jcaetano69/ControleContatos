970543bae9549ced6fa3eb2c617a0493f6a03acd
João Caetano Muniz Barros <joaocaetanomuniz1555@hotmail.com>
2025-04-10 15:53:41 -0300

    editar usuário finalizado,disparando erro caso tenha e com a mensagem de sucesso.

--- ControleContatos/Controllers/UsuarioController.cs
+++ ControleContatos/Controllers/UsuarioController.cs
@@ -24,6 +24,43 @@ namespace ControleContatos.Controllers
             return View();
         }
 
+        public IActionResult Edit(int id)
+        {
+            UsuarioModel usuario = _usuarioRepository.ListarPorId(id);
+            return View(usuario);
+        }
+
+        public IActionResult DeleteConfirm(int id)
+        {
+            UsuarioModel usuario = _usuarioRepository.ListarPorId(id);
+            return View(usuario);
+        }
+
+
+        public IActionResult Apagar(int id)
+        {
+            try
+            {
+                bool apagado = _usuarioRepository.Apagar(id);
+
+                if (apagado)
+                {
+                    TempData["MensagemSucesso"] = "Usuário apagado com sucesso!";
+                }
+
+                else
+                {
+                    TempData["MensagemErro"] = "Ops, não conseguimos apagar seu usuário.";
+                }
+                return RedirectToAction("Index");
+            }
+            catch (System.Exception erro)
+            {
+                TempData["MensagemErro"] = $"Ops, não conseguimos apagar seu usuários, mais detalhes do erro:{erro.Message}";
+                return RedirectToAction("Index");
+            }
+
+        }
 
         [HttpPost]
         public IActionResult Criar(UsuarioModel usuarios)
@@ -48,5 +85,40 @@ namespace ControleContatos.Controllers
 
         }
 
+
+
+
+        [HttpPost]
+        public IActionResult Alterar(UsuarioSemSenhaModel usuarioSemSenhaModel)
+        {
+            try
+            {
+                UsuarioModel usuario = null;
+                if (ModelState.IsValid)
+                {
+
+                    usuario = new UsuarioModel()
+                    {
+                        Id = usuarioSemSenhaModel.Id,
+                        Nome = usuarioSemSenhaModel.Nome,
+                        Login = usuarioSemSenhaModel.Login,
+                        Email = usuarioSemSenhaModel.Email,
+                        Perfil = usuarioSemSenhaModel.Perfil
+                    };
+                    usuario = _usuarioRepository.Atualizar(usuario);
+                    TempData["MensagemSucesso"] = "Usuário editado com sucesso";
+                    return RedirectToAction("Index");
+
+                }
+                return View( usuario);
+            }
+            catch (System.Exception error)
+            {
+
+                TempData["MensagemErro"] = $"Ops, não conseguimos atualizar seu usuário, tente novamente, detalhe do erro {error.Message}";
+                return RedirectToAction("Index");
+            }
+        }
     }
 }
+
--- ControleContatos/Models/UsuarioSemSenhaModel.cs
+++ ControleContatos/Models/UsuarioSemSenhaModel.cs
@@ -5,7 +5,7 @@ using System.Diagnostics;
 
 namespace ControleContatos.Models
 {
-    public class UsuarioModel
+    public class UsuarioSemSenhaModel
     {
         public int Id { get; set; }
         [Required(ErrorMessage = "Digite o nome do usuário")]
@@ -17,10 +17,8 @@ namespace ControleContatos.Models
         public string Email { get; set; }
         [Required(ErrorMessage = "Informe o perfil do usuário")]
         public PerfilEnum? Perfil { get; set; }
-        [Required(ErrorMessage = "Digite a senha do usuário")]
-        public string Senha { get; set; }
-        public DateTime DataCadastro { get; set; }
-        public DateTime? DataAlteracao { get; set; }
+     
+  
 
     }
 }
