4ff1a698adaebcd114583ee3cf1f4f5fcda5e4fe
João Caetano Muniz Barros <joaocaetanomuniz1555@hotmail.com>
2025-04-14 14:55:51 -0300

    layout e model do login

---
+++ ControleContatos/Controllers/LoginController.cs
@@ -0,0 +1,34 @@
+﻿using ControleContatos.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace ControleContatos.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Entrar (LoginModel loginModel)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                return View("Index");
+            }
+            catch (Exception erro)
+            {
+
+                TempData["MensagemErro"] = $"Ops, não conseguimos realizar seu login, tente novamente, mais detalhes do erro:{erro.Message}";
+                return RedirectToAction("Index");
+            }
+        }
+
+    }
+}
